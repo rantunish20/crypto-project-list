0063c65885e88ba035651cdc640177e548cfd9ca
Romel <rantunish20@gmail.com>
2021-06-13 18:04:46 -0400

    adding files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import Header from './components/Header';
 
 function App() {
+  const[coins, coinInfo] = useState([]);
+
+useEffect(() => {
+  fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=25&page=1&sparkline=false")
+  .then((r) => r.json())
+  .then((data) => {
+    coinInfo(data);
+    console.log(data);
+  } )
+
+}, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>List Your Favorite Crypto!</h1>
+      <Header />
+      {coins.map((data) => (
+        <div className="coinList" key={data.id}>
+          <p>
+            <img src={data.image} alt={data.name}/>
+            {data.symbol} | 
+            {data.name} |
+            Price: {data.current_price}
+            <button>Add to Fav</button>
+          </p>
+        </div>
+      ))}
     </div>
   );
+
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header () {
+    return(
+        <nav>
+            <button> My List </button>
+            <button> Crypto List </button>
+        </nav>
+    )
+}
+
+export default Header; 
