902d9d6ad2e3bf209bdf2ed45d7fd494715691c0
Romel <rantunish20@gmail.com>
2021-06-21 18:36:51 -0400

    Lauch app

--- src/App.js
+++ src/App.js
@@ -10,14 +10,14 @@ function App() {
   const[search, setSearch] = useState ('');
   const[show, setShow] = useState(true);
   const[popup, setPopUp] = useState(false);
-  const[currentprice, setCurrentPrice] = useState();
+  
 
   useEffect(() => {
       fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false")
       .then((r) => r.json())
       .then((data) => {
         coinInfo(data);
-        setCurrentPrice(data.current_price)
+        
         console.log(data);
       } )
     }, []);
